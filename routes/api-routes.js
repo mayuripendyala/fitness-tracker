9a2354538a46b118c4318725bcc1a5f99b342f1f
Mayuri Pendyala <mayu04@gmail.com>
2021-09-25 18:53:32 +1000

    updated api route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -15,3 +15,52 @@
             });
 
  });
+
+ router.post ("/api/workouts/range",(req,res) => {
+     db.create(body)
+     .then((dbData) =>{
+         res.json(dbData);
+     })
+     .catch((err) =>{
+        res.json(err);
+    });
+ });
+
+
+ router.get("api/workouts/range",(req,res) => {
+
+    db.find()
+            .then((dbData) =>{
+                res.json(dbData);
+            })
+            .catch((err) =>{
+                res.json(err);
+            });
+ });
+
+
+ router.post("/api/workouts",({ body },res)=>{
+            db.create(body)
+                        .then((dbData) =>{
+                            res.json(dbData);
+                        })
+                        .catch((err) =>{
+                            res.json(err);
+                        });
+
+ });
+
+
+ router.put("/api/workouts/:id", ({body, params }, res)=>{
+        db.findByIdAndUpdate(params.id,{$push :{ exercise :body}})
+                    .then((dbData) =>{
+                        res.json(dbData);
+                    })
+                    .catch((err) =>{
+                        res.json(err);
+                    });
+
+ });
+
+
+ module.exports = router;
